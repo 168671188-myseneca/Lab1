cf6bffb7e2a8d6e04e46647684b617ec47d37115
168671188-myseneca <dmorales5@myseneca.ca>
2024-02-22 03:47:00 +0100

    Lab1 Update

--- lab1.py
+++ lab1.py
@@ -2,26 +2,21 @@
 
 from datetime import datetime, timedelta
 
-def helloWorld():
-    print("Hello world!")
-
-helloWorld()
-
 # Obtain the present date
 now = datetime.now()
 
 # Prompt the user for their date of birth
 print("Please provide your date of birth (YYYY-MM-DD): ")
 dob = input()
+while True:
+    try:
+        birthday = datetime.strptime(dob, "%Y-%m-%d")
+        timeDifference = now - birthday
+        age_in_years = timeDifference.days // 365
+        break
+    except(ValueError):
+        birthday = input("Please provide your date of birth in the respected format (YYYY-MM-DD): ")
 
-# Convert the user's input into datetime 
-birthday = datetime.strptime(dob, "%Y-%m-%d")
-
-# Calculate the difference between the present and birthday
-timeDifference = now - birthday
-
-# Determine the person's age via years
-age_in_years = timeDifference.days // 365
 
 print(f"You're {age_in_years} years old!")
 
